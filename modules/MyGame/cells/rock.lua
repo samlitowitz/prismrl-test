799407e671d1837a13f270d1e4c9724e579f6530
Sam L <samlitowitz@users.noreply.github.com>
2025-05-04 09:42:03 -0400

    Walls are rocks

---
+++ modules/MyGame/cells/rock.lua
@@ -0,0 +1,13 @@
+--- @class RockCell : Cell
+local Rock = prism.Cell:extend("RockCell")
+Rock.name = "Rock" -- displayed in the user interface
+
+function Rock:initialize()
+  return {
+    prism.components.Drawable(string.byte("#") + 1),
+    prism.components.Collider({ allowedMoveTypes = {"fly"}}),
+    prism.components.Opaque()
+  }
+end
+
+return Rock
